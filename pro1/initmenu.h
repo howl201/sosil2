a83d97c461670be8ce86fbc6a2c619ecfb36c44e
howl201 <skatjdehddms@gmail.com>
2022-11-04 16:47:33 +0900

    project

---
+++ pro1/initmenu.h
@@ -0,0 +1,7 @@
+#ifndef __INITMENU_H__
+#define __INITMUNU_H__
+void initmenu1();
+void initmenu2();
+void initmenu3();
+int initselectmenu();
+#endif
---
+++ pro1/mainmenu.h
@@ -0,0 +1,7 @@
+#ifndef __MAINMENU_H__
+#define __MAINMENU_H__
+int mainselect();
+void mainworker();
+void mainprint();
+void mainexit();
+#endif
